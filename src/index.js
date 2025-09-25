51d8a805fe8d2a686d0e19bc6d151e99205bff83
Azamshoukat003 <azamsniper1122@gmail.com>
2025-09-25 15:34:10 +0500

    changes occur

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`server is running at port ${process.env.PORT}`);
-        });
+        // app.listen(process.env.PORT || 8000, () => {
+        //     console.log(`server is running at port ${process.env.PORT}`);
+        // });
     })
     .catch((err) => {
         console.log("mogo db connection Failed!!!", err);
