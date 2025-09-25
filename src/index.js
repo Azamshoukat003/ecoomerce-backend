cb644ecdae16c681f1b2f82e446a91167a4c29ba
Azamshoukat003 <azamsniper1122@gmail.com>
2025-09-25 16:00:57 +0500

    changes occur1

--- src/index.js
+++ src/index.js
@@ -1,38 +1,54 @@
+// import dotenv from "dotenv";
+// import connectDB from "./db/index.js";
+// import serverless from "serverless-http";
+// import { app } from "./app.js";
+
+// dotenv.config({
+//     path: "./env",
+// });
+
+// connectDB()
+//     .then(() => {
+//         // app.listen(process.env.PORT || 8000, () => {
+//         //     console.log(`server is running at port ${process.env.PORT}`);
+//         // });
+//         return;
+//     })
+//     .catch((err) => {
+//         console.log("mogo db connection Failed!!!", err);
+//     });
+
+// export default serverless(app);
+
+// // const app = express()(async () => {
+// //     try {
+// //         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+// //         app.on("error", (error) => {
+// //             console.log("error in back");
+// //             throw error;
+// //         });
+
+// //         app.listen(process.env.PORT, () => {
+// //             console.log("app is listening ib prt", process.env.PORT);
+// //         });
+// //     } catch (error) {
+// //         console.error("ERROR", error);
+// //         throw error;
+// //     }
+// // })();
+
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import serverless from "serverless-http";
 import { app } from "./app.js";
 
-dotenv.config({
-    path: "./env",
+dotenv.config({ path: "./env" });
+
+// connect to DB before handling requests
+connectDB().catch((err) => {
+  console.log("MongoDB connection failed:", err);
 });
 
-connectDB()
-    .then(() => {
-        // app.listen(process.env.PORT || 8000, () => {
-        //     console.log(`server is running at port ${process.env.PORT}`);
-        // });
-        return;
-    })
-    .catch((err) => {
-        console.log("mogo db connection Failed!!!", err);
-    });
-
+// Export serverless function for Vercel
 export default serverless(app);
 
-// const app = express()(async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.log("error in back");
-//             throw error;
-//         });
-
-//         app.listen(process.env.PORT, () => {
-//             console.log("app is listening ib prt", process.env.PORT);
-//         });
-//     } catch (error) {
-//         console.error("ERROR", error);
-//         throw error;
-//     }
-// })();
